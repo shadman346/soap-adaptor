b2bf76631ecfd50953c374628458d820338b3192
shadman346 <shadmanansari346c@gmail.com>
2023-01-09 19:00:28 +0530

    update_02

--- src/main/java/com/practice/soapadaptor/client/SOAPClientHelper.java
+++ src/main/java/com/practice/soapadaptor/client/SOAPClientHelper.java
@@ -1,5 +1,29 @@
 package com.practice.soapadaptor.client;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+
+import java.util.List;
+
 public class SOAPClientHelper {
+    private static void traverse(JsonNode node) {
+        if (node.getNodeType() == JsonNodeType.OBJECT) {
+        } else {
+            throw new RuntimeException("Not yet implemented");
+        }
+    }
+
+    private static void traverseObject(JsonNode node, List<String> list) {
+        node.fieldNames().forEachRemaining((String fieldName) -> {
+            JsonNode childNode = node.get(fieldName);
+            if (traversable(childNode)) {
+                traverseObject(childNode, list);
+            }
+        });
+    }
+
+    private static boolean traversable(JsonNode node) {
+        return node.getNodeType() == JsonNodeType.OBJECT;
+    }
 
 }
