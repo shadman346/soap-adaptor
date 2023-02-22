3f4a55ed0a78267b724b571b1607e8ae43a8fd6c
Shadman.Ansari <shadmanansari346c@gmail.com>
2023-02-23 02:43:07 +0530

    update

---
+++ src/main/java/com/healthedge/payor/core/adaptor/DTO/request/GetLatestVersionOfClaimWithNoLogEntry.java
@@ -0,0 +1,10 @@
+package com.healthedge.payor.core.adaptor.DTO.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetLatestVersionOfClaimWithNoLogEntry {
+    Integer claimId;
+}
