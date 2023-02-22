6b8fd232d8efb67af93c8bfb5ab8da4ab1b5668c
shadman346 <shadmanansari346c@gmail.com>
2023-02-22 19:22:44 +0530

    update

---
+++ src/main/java/com/healthedge/payor/core/adaptor/dto/request/Request.java
@@ -0,0 +1,4 @@
+package com.healthedge.payor.core.adaptor.dto.request;
+
+public class Request {
+}
---
+++ src/main/java/com/healthedge/payor/core/adaptor/dto/response/Response.java
@@ -0,0 +1,4 @@
+package com.healthedge.payor.core.adaptor.dto.response;
+
+public class Response {
+}
